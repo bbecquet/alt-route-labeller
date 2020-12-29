c4833ad61dfade9cc41fae5c97217831414afbe1
bbecquet <benjamin.becquet@gmail.com>
2020-12-29 23:33:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# alt-route-labeller
+
+A library to compute the best position to put labels on multiple routes between two points.
+
---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: 'index.js',
+  output: {
+    file: 'dist/altRouteLabeller.js',
+    format: 'umd',
+    name: 'altRouteLabeller',
+  },
+  plugins: [
+    commonjs(),
+    nodeResolve({ preferBuiltins: false }),
+    babel({ babelHelpers: 'bundled' }),
+    terser(),
+  ]
+};
