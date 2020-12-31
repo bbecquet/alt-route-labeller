65bba8af0f0892e693084bfe40dfdc7c9edeb006
bbecquet <benjamin.becquet@gmail.com>
2020-12-31 12:35:11 +0100

    Introduce functional-style programming with Ramda

--- index.js
+++ index.js
@@ -4,21 +4,25 @@ import { getCoord } from '@turf/invariant';
 import along from '@turf/along';
 import lineLength from '@turf/length';
 import bearing from '@turf/bearing';
+import * as R from 'ramda';
+
+const TOLERANCE = 0.000001;
+const floatEquals = (f1, f2) => Math.abs(f1 - f2) < TOLERANCE;
+const coordEquals = (c1 = [], c2 = []) => floatEquals(c1[0], c2[0]) && floatEquals(c1[1], c2[1]);
+const asKey = coord => `${coord[0].toFixed(6)},${coord[1].toFixed(6)}`;
 
 // find the point at the given distance ratio on the linestring
-function project(ratio, ls) {
+const project = R.curry((ratio, ls) => {
   const length = lineLength(ls);
   const lngLat = getCoord(along(ls, length * ratio));
-  /// keep the local bearing of the line to later choose an anchor minimizing the portion of line covered.
+  // keep the local bearing of the line to later choose an anchor minimizing the portion of line covered.
   const localLineBearing = bearing(
     along(ls, length * (ratio - 0.1)),
     along(ls, length * (ratio + 0.1))
   );
 
   return { lngLat, localLineBearing };
-}
-
-const asKey = coord => `${coord[0].toFixed(6)},${coord[1].toFixed(6)}`;
+});
 
 function distinctSegment(coordinates, coordCounts) {
   // a distinct segment is a part of a line where coordinates
@@ -52,22 +56,11 @@ export function findDistinctSegments(linestrings) {
   return featuresCoords.map(coordinates => distinctSegment(coordinates, coordCounts));
 }
 
-const toSimpleLinestring = geoJson => lineString(dropRepeatedCoords(coordAll(geoJson)));
-
-const TOLERANCE = 0.000001;
-const floatEquals = (f1, f2) => Math.abs(f1 - f2) < TOLERANCE;
-
-const coordEquals = (c1 = [], c2 = []) => floatEquals(c1[0], c2[0]) && floatEquals(c1[1], c2[1]);
-
-function dropRepeatedCoords(list) {
-  const result = [];
-  for (let i = 0; i < list.length; i++) {
-    if (!coordEquals(result[result.length - 1], list[i])) {
-      result.push(list[i]);
-    }
-  }
-  return result;
-}
+const toSimpleLinestring = R.pipe(
+  coordAll,
+  R.dropRepeatsWith(coordEquals),
+  lineString,
+);
 
 // Reduce possibilities of collision by chosing anchors so that labels repulse each other
 function optimizeAnchors(positions) {
@@ -85,13 +78,11 @@ function optimizeAnchors(positions) {
 }
 
 function getBearingFromOtherPoints(position, others) {
-  if (others.length === 0) {
-    return 0;
-  }
-  return others
-    .map(other => bearing(other.lngLat, position.lngLat))
-    .reduce((sum, current) => sum + current)
-    / others.length;
+  return R.pipe(
+    R.map(other => bearing(other.lngLat, position.lngLat)),
+    R.mean,
+    R.defaultTo(0),
+  )(others);
 }
 
 function getAnchor(position, otherBearing) {
@@ -106,9 +97,11 @@ function getAnchor(position, otherBearing) {
 }
 
 export function getLabelPositions(featureCollection) {
-  const lineStrings = featureCollection.features.map(toSimpleLinestring);
-  const distinctSegments = findDistinctSegments(lineStrings);
-  const positions = distinctSegments.map(branchCoords => project(0.5, branchCoords));
-  const optimizedPositions = optimizeAnchors(positions);
-  return optimizedPositions;
+  return R.pipe(
+    R.prop('features'),
+    R.map(toSimpleLinestring),
+    findDistinctSegments,
+    R.map(project(0.5)),
+    optimizeAnchors,
+  )(featureCollection);
 }
